97be453d419583e04c54f27875d9504f9d243cbc
choyunju <yoonj1860@naver.com>
2024-09-30 17:14:00 +0900

    [refactoring] 디자인 수정 & 블루투스 연동 코드 수정

--- app/src/main/java/com/example/smartFarm/ControlActivity.kt
+++ app/src/main/java/com/example/smartFarm/ControlActivity.kt
@@ -5,12 +5,16 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.smartFarm.databinding.ActivityControlBinding
 import okio.IOException
+import java.io.InputStream
+import java.io.OutputStream
 import java.util.UUID
 
 class ControlActivity : AppCompatActivity() {
@@ -18,7 +22,10 @@ class ControlActivity : AppCompatActivity() {
     private val binding get() = mbinding!!
     private var bluetoothAdapter: BluetoothAdapter? = null
     private var bluetoothSocket: BluetoothSocket? = null
-    private val deviceAddress = "00:11:22:33:AA:BB" //연결할 Arduino의 Bluetooth 주소
+    //    private val deviceAddress = "00:11:22:33:AA:BB" //연결할 Arduino의 Bluetooth 주소
+    private var outputStream: OutputStream? = null
+    private var inputStream: InputStream? = null
+    private val deviceName = "HC-06"
     private val uuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB") //HC-05의 UUID
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,14 +35,60 @@ class ControlActivity : AppCompatActivity() {
 
         //Bluetooth 연결 시도
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (checkPermissions()) {
+            initBluetooth()
+        } else {
+            request()
+        }
         connectBluetooth()
-
+        Log.d("controlActivity","process")
         setupClickListener()
     }
 
-    private fun connectBluetooth() {
-        val device: BluetoothDevice? = bluetoothAdapter?.getRemoteDevice(deviceAddress)
+    private fun checkPermissions(): Boolean{
+        Log.d("checkpermissions", "yes")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            return ContextCompat.checkSelfPermission(
+                this, Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_GRANTED
+        }
 
+        return ContextCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun request() {
+        Log.d("request", "yes")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                1
+            )
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        }
+    }
+
+
+
+    private fun initBluetooth() {
+        Log.d("initbluetooth", "yes")
+        if(bluetoothAdapter == null) {
+            Log.d("Bluetooth", "not supported in this device")
+        } else if(!bluetoothAdapter!!.isEnabled){
+            Log.d("bluetooth", "please enable Bluetooth")
+        }
+        return
+    }
+
+    private fun connectBluetooth() {
+        Log.d("connectBluetooth", "yes")
         try {
             if (ActivityCompat.checkSelfPermission(
                     this,
@@ -44,11 +97,41 @@ class ControlActivity : AppCompatActivity() {
             ) {
                 return
             }
-            bluetoothSocket = device?.createRfcommSocketToServiceRecord(uuid)
-            bluetoothSocket?.connect()
+            val pairedDevices: Set<BluetoothDevice> = bluetoothAdapter?.bondedDevices ?: emptySet()
+
+            for(device in pairedDevices) {
+                if(device.name == deviceName) {
+                    try {
+                        bluetoothSocket = device.createRfcommSocketToServiceRecord(uuid)
+                        bluetoothSocket?.connect()
+                        outputStream = bluetoothSocket?.outputStream
+                        inputStream = bluetoothSocket?.inputStream
+                        Log.d("bluetooth", "connected to HC-06")
+                    }    catch (e: IOException) {
+                        Log.d("bluetooth", "failed")
+                    }
+                }
+
+            }
             Log.d("bluetooth connect", "success")
         } catch(e: IOException) {
             Log.d("bluetooth", "fail")
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 1) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initBluetooth()
+            } else {
+                Log.d("Bluetooth","permissions are required")
+            }
         }
     }
 
@@ -67,7 +150,14 @@ class ControlActivity : AppCompatActivity() {
 
         //물 공급 제어
         binding.waterControl.setOnClickListener {
-
+            val text = binding.waterControl.text.toString()
+            if(text == "ON") {
+                binding.waterControl.text = "OFF"
+                sendDataToArduino("E")
+            } else if(text == "OFF") {
+                binding.waterControl.text = "ON"
+                sendDataToArduino("W")
+            }
         }
 
         //LED 제어
@@ -78,10 +168,18 @@ class ControlActivity : AppCompatActivity() {
     }
 
     private fun sendDataToArduino(data: String) {
-        try {
-            bluetoothSocket?.outputStream?.write(data.toByteArray())
-        } catch(e: IOException) {
-            Log.d("sendDataToArduino", "fail")
+//        try {
+//            bluetoothSocket?.outputStream?.write(data.toByteArray())
+//        } catch(e: IOException) {
+//            Log.d("sendDataToArduino", "fail")
+//        }
+        if(bluetoothSocket!= null && outputStream != null) {
+            try {
+                outputStream?.write(data.toByteArray())
+                Log.d("data send", "success $data")
+            } catch (e: IOException) {
+                Log.d("data send", "fail")
+            }
         }
     }
 
--- app/src/main/java/com/example/smartFarm/MainActivity.kt
+++ app/src/main/java/com/example/smartFarm/MainActivity.kt
@@ -1,39 +1,198 @@
 package com.example.smartFarm
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.lifecycle.ViewModelProvider
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.smartFarm.databinding.ActivityMainBinding
-import okhttp3.OkHttp
-import okhttp3.OkHttpClient
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
-    private var mBinding : ActivityMainBinding? = null
-    private val binding get() = mBinding!!
+    private var mbinding: ActivityMainBinding? = null
+    private val binding get() = mbinding!!
 
-    private lateinit var controlViewModel: ControlViewModel
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    private var bluetoothSocket: BluetoothSocket? = null
+    private var outputStream: OutputStream? = null
+    private var inputStream: InputStream? = null
+    private val deviceName = "HC-06"
+    private val uuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB") //HC-06의 UUID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        //Bluetooth 권한 체크
+        if (checkPermissions()) {
+            initBluetooth()
+        } else {
+            request()
+        }
 
-        controlViewModel = ViewModelProvider(this)[ControlViewModel::class.java]
-
-        setupClickListener()
+        Log.d("MainActivity","process")
+        setupControlListener()
     }
 
-    private fun setupClickListener() {
-        binding.switchVentilator.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) {
-                val text = "/ventilatorOn"
-                sendControl(text)
+    private fun checkPermissions(): Boolean{
+        Log.d("checkpermissions", "yes")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            return ContextCompat.checkSelfPermission(
+                this, Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+        return ContextCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun request() {
+        Log.d("request", "yes")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                1
+            )
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        }
+    }
+
+    private fun initBluetooth() {
+        Log.d("initbluetooth", "yes")
+        if(bluetoothAdapter == null) {
+            Log.d("Bluetooth", "not supported in this device")
+        } else if(!bluetoothAdapter!!.isEnabled){
+            Log.d("bluetooth", "please enable Bluetooth")
+        } else {
+            connectBluetooth()
+        }
+        return
+    }
+
+    private fun connectBluetooth() {
+        Log.d("connectBluetooth", "yes")
+        try {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            val pairedDevices: Set<BluetoothDevice> = bluetoothAdapter?.bondedDevices ?: emptySet()
+
+            for(device in pairedDevices) {
+                if(device.name == deviceName) {
+                    try {
+                        bluetoothSocket = device.createRfcommSocketToServiceRecord(uuid)
+                        bluetoothSocket?.connect()
+                        outputStream = bluetoothSocket?.outputStream
+                        inputStream = bluetoothSocket?.inputStream
+                        Log.d("bluetooth", "connected to HC-06")
+                    }    catch (e: IOException) {
+                        Log.d("bluetooth", "failed")
+                    }
+                }
+
+            }
+            Log.d("bluetooth connect", "success")
+        } catch(e: IOException) {
+            Log.d("bluetooth", "fail")
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 1) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initBluetooth()
+            } else {
+                Log.d("Bluetooth","permissions are required")
             }
         }
     }
 
-    private fun sendControl(text: String) {
-        controlViewModel.controlVentilator(text)
+    private fun setupControlListener() {
+        binding.fanSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                binding.fanFrame.setBackgroundResource(R.drawable.fill_border)
+                sendData("0")
+            }
+            else {
+                binding.fanFrame.setBackgroundResource(R.drawable.border)
+                sendData("1")
+            }
+        }
+
+        binding.waterSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                binding.waterFrame.setBackgroundResource(R.drawable.fill_border)
+                sendData("W")
+            }
+            else {
+                binding.waterFrame.setBackgroundResource(R.drawable.border)
+                sendData("E")
+            }
+        }
+
+        binding.ledSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                binding.ledFrame.setBackgroundResource(R.drawable.fill_border)
+            }
+            else {
+                binding.ledFrame.setBackgroundResource(R.drawable.border)
+            }
+        }
+    }
+
+    private fun sendData(data: String) {
+        if(bluetoothSocket!= null && outputStream != null) {
+            try {
+                outputStream?.write(data.toByteArray())
+                Log.d("data send", "success $data")
+            } catch (e: IOException) {
+                Log.d("data send", "fail")
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            bluetoothSocket?.close()
+        } catch (e: IOException) {
+            Log.d("socket close", "fail")
+        }
     }
 }
